d07b663ee4624ef097636854104c9ea9a4181010
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-07-16 01:24:11 +0500

    UesEffect hook to remove sideeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useRef } from "react";
+
 function App() {
-  return <></>;
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+
+  return (
+    <>
+      <input ref={ref} type="text" className="form-control" />
+    </>
+  );
 }
 
 export default App;
