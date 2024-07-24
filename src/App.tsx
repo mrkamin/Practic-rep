44d4cb0c521d05c8b0a987b19acf4da22813dd31
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-07-24 15:32:41 +0500

    Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  axios
-    .get("https://jsonplaceholder.typicode.com/users")
-    .then((res) => console.log(res.data));
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
   return (
     <>
-      <div className="mb-5"></div>
+      <div className="mb-5">
+        <ul className="list-group">
+          {users.map((user) => (
+            <li key={user.id} className="list-item">
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
