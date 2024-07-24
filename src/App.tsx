6cbcc36b13af37a838fb0b3971f1fdc573753a5f
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-07-24 15:22:28 +0500

    Use the get method of axios to get data

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
-import Productlist from "./components/Productlist";
+import axios from "axios";
 
 function App() {
-  const connect = () => console.log("Connecting");
-  const disconnect = () => console.log("Disconnecting");
-  useEffect(() => {
-    connect();
-    return () => disconnect();
-  });
-  const [category, setCategory] = useState("");
+  axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => console.log(res.data));
   return (
     <>
-      <div className="mb-5">
-        <select
-          className="form-select"
-          onChange={(event) => setCategory(event.target.value)}
-        >
-          <option value=""></option>
-          <option value="Clouthing">Clouthing</option>
-          <option value="Household">Household</option>
-        </select>
-        <Productlist category={category} />
-      </div>
+      <div className="mb-5"></div>
     </>
   );
 }
